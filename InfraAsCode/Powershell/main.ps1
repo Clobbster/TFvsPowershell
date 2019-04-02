f3ace040c0a3798bf28afc9cbdcbcc4c5fec5292
Christian <christian.rowden@greyhound.com>
2019-04-02 11:03:44 -0500

    'Issues'

---
+++ InfraAsCode/Powershell/main.ps1
@@ -0,0 +1,93 @@
+﻿###########################################################
+# Subscription
+###########################################################
+$subscription = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+az account set --subscription $subscription
+
+###########################################################
+# Resource Group
+###########################################################
+$rgName   = "RG-DDoA"
+$location = "SouthCentralUS"
+
+$resourceGroup = az group create --name $rgName --location $location
+
+###########################################################
+# Storage Account
+###########################################################
+$storageAcctName = ”ddoastrg”
+$storageAcctType = “Standard_LRS”
+
+$storageAcct = az storage account create --name $storageAcctName --resource-group $rgName --location $location
+
+###########################################################
+# Availability Set
+###########################################################
+az vm availability-set create --name "TCAvailabilitySet1" --resource-group $rgName --location $location
+
+###########################################################
+# Load Balancer
+###########################################################
+
+# Public IP
+az network public-ip create --name "TCLBpip" --location $location --resource-group $rgName --allocation-method Static
+
+# Load Balancer
+az network lb create --name "TCLB" --location $location --resource-group $rgName --public-ip-address "TCLBpip"
+
+# Probe
+az network lb probe create --lb-name "TCLB" --name "TCLBprobe" --resource-group $rgName --port 80 --protocol "Tcp"
+
+# Backend Address Pool
+az network lb address-pool create --lb-name "TCLB" --name "BackEndAddressPool" --resource-group $rgName
+ 
+# LB Rule: Port 80
+az network lb rule create --frontend-port 80 --backend-port 80 --lb-name "TCLB" --name "LBRule80" --protocol "Tcp" --resource-group $rgName --frontend-ip-name "LoadBalancerFrontEnd" --backend-pool-name "BackEndAddressPool" --probe-name "TCLBprobe"
+
+###########################################################
+# NICs
+###########################################################
+
+# TC1 PIP
+az network public-ip create --name "TC1pip" --location $location --resource-group $rgName --allocation-method Static
+
+# TC1 NIC
+$TC1NIC = az network nic create --name "TC1Nic" --resource-group $rgName --location $location --subnet "/subscriptions/68cf210d-efb0-4615-8b0f-20ae6c9ff0c3/resourceGroups/NON-PROD-NETWORK/providers/Microsoft.Network/virtualNetworks/NON-PROD-NETWORK-vnet/subnets/default" `
+        --lb-address-pools "BackEndAddressPool" --lb-name "TCLB" --public-ip-address "TC1pip"
+
+# TC2 PIP
+az network public-ip create --name "TC2pip" --location $location --resource-group $rgName --allocation-method Static
+
+# TC2 NIC
+$TC2NIC = az network nic create --name "TC2Nic" --resource-group $rgName --location $location --subnet "/subscriptions/68cf210d-efb0-4615-8b0f-20ae6c9ff0c3/resourceGroups/NON-PROD-NETWORK/providers/Microsoft.Network/virtualNetworks/NON-PROD-NETWORK-vnet/subnets/default" `
+        --lb-address-pools "BackEndAddressPool" --lb-name "TCLB" --public-ip-address "TC2pip"
+
+###########################################################
+# VMs
+###########################################################
+# Update for your admin password
+$AdminPassword = "Password1234!"
+
+# TC1
+$VM1 = az vm create --resource-group $rgName --name "TC1" --location $location --nics "TC1Nic" --size Standard_D1_v2 --image UbuntuLTS --admin-username testadmin --admin-password $AdminPassword --availability-set "TCAvailabilitySet1"
+
+# TC2
+$VM1 = az vm create --resource-group $rgName --name "TC2" --location $location --nics "TC2Nic" --size Standard_D1_v2 --image UbuntuLTS --admin-username testadmin --admin-password $AdminPassword --availability-set "TCAvailabilitySet1"
+
+
+# Future Goals: Custom Script on provision that installs Apache2 and modifies the files to display DDoA default site. 
+
+
+#ssh adminaccount@ipofhost
+#sudo apt-get update
+#sudo apt-get install apache2
+#sudo systemctl start apache2
+#cd /var/www/html
+#sudo chmod 777 index.html
+#Add text to denote TC1 and TC2
+#
+#Browse to public ip of TC1 and TC2: Apache2 Ubuntu Default Page should be displayed and denoted
+#
+#Browse to public ip of TCLB: Apache2 Ubuntu Default Page should be displayed denoting which server you've connected to
+#Turn off the server that you've connected to and refresh the Apache2 Ubuntu Default Page which should now denote the second server
---
+++ InfraAsCode/Terraform/main.tf
@@ -0,0 +1,236 @@
+#############################################
+# Resource Group
+#############################################
+resource "azurerm_resource_group" "RG-DDoA" {
+  name     = "RG-DDoA"
+  location = "SouthCentralUS"
+}
+
+#############################################
+# Storage Account
+#############################################
+resource "azurerm_storage_account" "ddoastrg" {
+  name                     = "ddoastrg"
+  resource_group_name      = "${azurerm_resource_group.RG-DDoA.name}"
+  location                 = "${azurerm_resource_group.RG-DDoA.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#############################################
+# Availability Set
+#############################################
+resource "azurerm_availability_set" "TCAvailabilitySet1" {
+  name                = "TCAvailabilitySet1"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+  managed             = true
+}
+
+#############################################
+# Load Balancer
+#############################################
+resource "azurerm_public_ip" "TCLBpip" {
+  name                = "TCLBpip"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "TCLB" {
+  name                = "TCLB"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.TCLBpip.id}"
+  }
+}
+
+resource "azurerm_lb_probe" "TCLBprobe" {
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+  loadbalancer_id     = "${azurerm_lb.TCLB.id}"
+  name                = "TCLBprobe"
+  port                = 80
+}
+
+resource "azurerm_lb_backend_address_pool" "TCLBbep" {
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+  loadbalancer_id     = "${azurerm_lb.TCLB.id}"
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_rule" "LBRule80" {
+  resource_group_name            = "${azurerm_resource_group.RG-DDoA.name}"
+  loadbalancer_id                = "${azurerm_lb.TCLB.id}"
+  name                           = "LBRule80"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.TCLBbep.id}"
+  probe_id                       = "${azurerm_lb_probe.TCLBprobe.id}"
+}
+
+#############################################
+# Pull subnet information from Azure
+#############################################
+data "azurerm_subnet" "default" {
+  name                 = "default"
+  virtual_network_name = "NON-PROD-NETWORK-vnet"
+  resource_group_name  = "NON-PROD-NETWORK"
+}
+
+output "subnet_id" {
+  value = "${data.azurerm_subnet.default.id}"
+}
+
+#############################################
+# NICs
+#############################################
+
+# TC1 Nic
+resource "azurerm_public_ip" "TC1pip" {
+  name                = "TC1pip"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "TC1Nic" {
+  name                = "TC1Nic"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+
+  ip_configuration {
+    name                                    = "tc-ip-config"
+    subnet_id                               = "${data.azurerm_subnet.default.id}"
+    public_ip_address_id                    = "${azurerm_public_ip.TC1pip.id}"
+    private_ip_address_allocation           = "Dynamic"
+    load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.TCLBbep.id}"]
+  }
+}
+
+# TC2 Nic 
+resource "azurerm_public_ip" "TC2pip" {
+  name                = "TC2pip"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "TC2Nic" {
+  name                = "TC2Nic"
+  location            = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name = "${azurerm_resource_group.RG-DDoA.name}"
+
+  ip_configuration {
+    name                                    = "tc-ip-config"
+    subnet_id                               = "${data.azurerm_subnet.default.id}"
+    public_ip_address_id                    = "${azurerm_public_ip.TC2pip.id}"
+    private_ip_address_allocation           = "Dynamic"
+    load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.TCLBbep.id}"]
+  }
+}
+
+#############################################
+# VMs
+#############################################
+
+# TC1
+resource "azurerm_virtual_machine" "TC1" {
+  name                  = "TC1"
+  location              = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name   = "${azurerm_resource_group.RG-DDoA.name}"
+  network_interface_ids = ["${azurerm_network_interface.TC1Nic.id}"]
+  vm_size               = "Standard_DS1_v2"
+  availability_set_id   = "${azurerm_availability_set.TCAvailabilitySet1.id}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "TC1disk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "TC1"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+# TC2
+resource "azurerm_virtual_machine" "TC2" {
+  name                  = "TC2"
+  location              = "${azurerm_resource_group.RG-DDoA.location}"
+  resource_group_name   = "${azurerm_resource_group.RG-DDoA.name}"
+  network_interface_ids = ["${azurerm_network_interface.TC2Nic.id}"]
+  vm_size               = "Standard_DS1_v2"
+  availability_set_id   = "${azurerm_availability_set.TCAvailabilitySet1.id}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "TC2disk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "TC2"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
+# Future Goals: Custom Script on provision that installs Apache2 and modifies the files to display DDoA default site. 
+
+
+#ssh adminaccount@ipofhost
+#sudo apt-get update
+#sudo apt-get install apache2
+#sudo systemctl start apache2
+#cd /var/www/html
+#sudo chmod 777 index.html
+#Add text to denote TC1 and TC2
+#
+#Browse to public ip of TC1 and TC2: Apache2 Ubuntu Default Page should be displayed and denoted
+#
+#Browse to public ip of TCLB: Apache2 Ubuntu Default Page should be displayed denoting which server you've connected to
+#Turn off the server that you've connected to and refresh the Apache2 Ubuntu Default Page which should now denote the second server
+
