c8453d21ceadeab859b100005564baf0c10c90fb
Clobbster <36778957+Clobbster@users.noreply.github.com>
2021-08-13 09:48:45 -0400

    Update provider.tf

--- InfraAsCode/Terraform/provider.tf
+++ InfraAsCode/Terraform/provider.tf
@@ -2,6 +2,6 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.22.0"
 
-  subscription_id = "68cf210d-efb0-4615-8b0f-20ae6c9ff0c3"
-  tenant_id       = "01bdfa3b-98f1-4d8b-8d0b-8e77ed5a8147"
+  subscription_id = ""
+  tenant_id       = ""
 }
